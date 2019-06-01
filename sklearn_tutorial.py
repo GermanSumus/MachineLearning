60066d68308db47920d4b17b0b2380565c11490a
German Rodriguez <GermanRodriguezWork@Gmail.com>
2019-06-01 16:00:34 -0500

    Merged to single file

--- sklearn_tutorial.py
+++ sklearn_tutorial.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 seaborn.set()
 
+# Iris Dataset
 iris = load_iris()
 print(iris.keys())
 print(iris.data[:5])
@@ -14,6 +15,8 @@ print(iris.target.shape)
 print(iris.target)
 print(iris.target_names)
 
+# Iris Dataset Visualized In Matplotlib
+# adjust the two indexes to see different results
 x_index = 2
 y_index = 3
 formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
@@ -25,4 +28,34 @@ plt.ylabel(iris.feature_names[y_index])
 plt.show()
 
 
+# Machine Learning Example
+# selecting a learning problem
 from sklearn.linear_model import LinearRegression
+
+# estimator paramaters are set up when it is instantiated
+model = LinearRegression(normalize=True)
+
+# some example data quickly made
+x = np.arange(10)
+y = 2 * x + 1
+print(x)
+print(y)
+
+# visualize in matplotlib
+plt.plot(x, y, 'o')
+plt.yticks(np.arange(0, (y.max() / 5 + 1) * 5, 5))
+plt.xticks(np.arange(0, 11, 1))
+plt.grid(False)
+plt.show()
+
+# Format Data For Sklearn
+# np.newaxis will increase the dimension of the array by 1
+X = x[:, np.newaxis]
+print(X)
+print(y)
+print(model.fit(X, y))
+
+# Attributes ending with a '_' indicate a fit parameter
+print(model.coef_)
+print(model.intercept_)
+print(model._residues)
